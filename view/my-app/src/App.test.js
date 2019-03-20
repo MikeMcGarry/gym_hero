b7098565351e3c63af505ea68e2f44bdc4c9decf
Mike McGarry <mikemcgarry@live.com.au>
2019-03-20 23:28:41 +0000

    Further changes for production deployment

--- view/my-app/src/App.test.js
+++ view/my-app/src/App.test.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Charts from './Charts';
+import Volumes from './Volumes';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<Charts />, div);
+  ReactDOM.unmountComponentAtNode(div);
+  ReactDOM.render(<Volumes />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
