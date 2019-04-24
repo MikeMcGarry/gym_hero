884160dcf4d06e638bea61bf4284a95d52d53581
Mike McGarry <mikemcgarry@live.com.au>
2019-04-24 20:16:51 +0100

    Kubernetes Updated workoutfetcher to be able to be used outside AWS and updated controller deployment with environment variables for this

--- datasource/workoutfetcher.py
+++ datasource/workoutfetcher.py
@@ -1,11 +1,26 @@
 import boto3
 import botocore
+import os
 
 Bucket = "gym-hero-data"
 Key = "gym-hero-export.csv"
 outPutName = "gym-hero-export.csv"
 
-s3 = boto3.resource('s3')
+# Pass in the access credentials via environment variables
+AWS_SERVER_PUBLIC_KEY = os.getenv("AWS_SERVER_PUBLIC_KEY", None)
+AWS_SERVER_SECRET_KEY = os.getenv("AWS_SERVER_SECRET_KEY", None)
+
+# Check if the environment variables exist, they are only required for external access
+if AWS_SERVER_PUBLIC_KEY is not None and AWS_SERVER_SECRET_KEY is not None:
+    session = boto3.Session(
+        aws_access_key_id=AWS_SERVER_PUBLIC_KEY,
+        aws_secret_access_key=AWS_SERVER_SECRET_KEY,
+    )
+    s3 = session.resource('s3')
+
+# If no environment variables rely on a AWS role instead
+else:
+    s3 = boto3.resource('s3')
 
 try:
     s3.Bucket(Bucket).download_file(Key, outPutName)
