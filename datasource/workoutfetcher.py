8c931b6f795dcebd2d48a1514a4f6e217cf487cd
Mike McGarry <mikemcgarry@live.com.au>
2019-06-12 00:38:57 +0100

    Updated to use the GymHero API as the data source rather than the file from Amazon Web Services

--- datasource/workoutfetcher.py
+++ datasource/workoutfetcher.py
@@ -1,36 +1,136 @@
-import boto3
-import botocore
+import pandas as pd
+import json
+import urllib.request
 import os
-import time
 
-Bucket = "gym-hero-data"
-Key = "gym-hero-export.csv"
-outPutName = "data/gym-hero-export.csv"
+def get_data(username, app_token):
+    """
+    This function takes the username of a GymHero user and their app token and
+    returns all their workouts as a JSON object loaded into Python types.
 
-# Pass in the access credentials via environment variables
-AWS_SERVER_PUBLIC_KEY = os.getenv("AWS_SERVER_PUBLIC_KEY", None)
-AWS_SERVER_SECRET_KEY = os.getenv("AWS_SERVER_SECRET_KEY", None)
+    param (str) username: The username to get workouts for
+    param (str) app_token: The app token of the user
 
-# Check if the environment variables exist, they are only required for external access
-if AWS_SERVER_PUBLIC_KEY is not None and AWS_SERVER_SECRET_KEY is not None:
-    session = boto3.Session(
-        aws_access_key_id=AWS_SERVER_PUBLIC_KEY,
-        aws_secret_access_key=AWS_SERVER_SECRET_KEY,
-    )
-    s3 = session.resource('s3')
+    return (list): The list of nested dictionaries containing the workout details
+    """
 
-# If no environment variables rely on a AWS role instead
-else:
-    s3 = boto3.resource('s3')
+    # Get the workout data from the GymHero api
+    response = urllib.request.urlopen(
+        "https://api.fitty.co/users/{}/workouts?app_token={}".format(
+            username, app_token
+        )).read()
+    # Convert it to json
+    workout_data = json.loads(response.decode('utf-8'))
+
+    return workout_data
+
+def process_data(workout_data):
+    """
+    This function takes the workout data and processes to be ready to be served
+    by the api.
+
+    param (list) workout_data: The list of nested dictionaries containing the workout details
+
+    return (DataFrame) workouts_df: The dataframe containing the workouts
+    """
+
+    # Initialise an empty list to hold each set
+    sets = []
+
+    # Map the unit keys to the units
+    weight_mapping = {
+        0: '-',
+        1: 'lb',
+        2: 'kg'
+    }
+
+    # Iterate over each workout
+    for workout in workout_data:
+        # Get the workout details
+        workout_id = workout['id']
+        finished = workout['finished']
+        workout_name = workout['label']
+        workout_date = workout['date']
+        print ('Working on workout {}'.format(workout_id))
+        # Iterate over each exercise
+        for exercise in workout['exercises']:
+            # Get the exercise details
+            exercise_name = exercise['label']
+            exercise_units = weight_mapping[exercise['unit']]
+            # If there are no sets skip this exercise
+            if exercise['sets'] is None:
+                continue
+            # Iterate over the sets
+            for weight_set in exercise['sets']:
+
+                # Get the reps
+                reps = weight_set['reps']
+
+                # Get the weight if it exists
+                if 'weight' in weight_set:
+                    weight = weight_set['weight']
+                # Else set to 0
+                else:
+                    weight = 0
+
+                # Create the details for the current set
+                current_set = {
+                    'Workout #': workout_id,
+                    'Finished': finished,
+                    'Workout': workout_name,
+                    'Date': workout_date,
+                    'Exercise': exercise_name,
+                    'Unit': exercise_units,
+                    'Weight': weight,
+                    'Reps': reps
+                }
+
+                # Append it to the list
+                sets.append(current_set)
+
+    # Create a dataframe from the list of sets
+    workouts_df = pd.DataFrame(sets)
+
+    # Replace any null values with 0
+    workouts_df['Weight'] = workouts_df['Weight'].fillna(value=0)
+    workouts_df['Reps'] = workouts_df['Reps'].fillna(value=0)
+
+    # Ensure all lb are converted to kg
+    workouts_df['Unit'] = workouts_df['Unit'].apply(
+        lambda x: 'kg' if x == 'lb' else x)
+
+    # All other units as seconds
+    workouts_df['Unit'] = workouts_df['Unit'].apply(
+        lambda x: 'sec' if x == '-' else x)
+
+    # Trim the dates so that they become seconds since 1970 rather than milliseconds
+    workouts_df['Date'] = workouts_df['Date'].apply(
+        lambda x: int(str(x)[:-3]))
+
+    # Return the dataframe
+    return workouts_df
+
+def save_data(workouts_df, file_path):
+    """
+    This function saves a dataframe to a file
+
+    param (DataFrame) workouts_df: The data frame to save to a file
+    param (str) file_path: The path to the output file
+    """
+
+    workouts_df.to_csv(file_path)
+
 
 while True:
 
-    try:
-        s3.Bucket(Bucket).download_file(Key, outPutName)
-    except botocore.exceptions.ClientError as e:
-        if e.response['Error']['Code'] == "404":
-            print("The object does not exist.")
-        else:
-            raise
+    workout_data = get_data(
+        username=os.environ['GYM_HERO_USERNAME'],
+        app_token=os.environ['GYM_HERO_APP_TOKEN'])
 
-    time.sleep(3600)
+    workouts_df = process_data(workout_data=workout_data)
+
+    save_data(
+        workouts_df=workouts_df,
+        file_path="./data/gym-hero-export.csv"
+    )
+    time.sleep(600)
