1a904153e7683c45ee8565a8935b6d87e8b0d252
Mike McGarry <mikemcgarry@live.com.au>
2019-11-02 17:13:29 +0000

    Updated dependencies

--- view/my-app/src/Charts.js
+++ view/my-app/src/Charts.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import axios from 'axios'
-import querystring from 'querystring';
+import querystring from 'query-string';
 import {
   LineChart,
   Line,
